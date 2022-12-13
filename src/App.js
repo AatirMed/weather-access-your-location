9208a685e4ed1c8afdbacf6c358df69e9d28feaa
AATIR Mohamed <105876846+AatirMed@users.noreply.github.com>
2022-12-13 23:06:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React, { Fragment} from "react";
-import { Link, Routes, Route } from "react-router-dom";
-import Exercice01 from './component/Exercice01';
-import Exercice02 from "./component/Exercice02";
+import React, { useState } from "react";
 import './App.css';
+import img1 from './Day01img/Day1Card01.jpg';
+import img2 from './Day01img/Day1Card02.jpg';
+import img3 from './Day01img/Day1Card03.jpg';
+import img4 from './Day01img/Day1Card04.jpg';
+import img5 from './Day01img/Day1Card05.jpg';
 
 const App = () => {
 
+  const All_img = [img1, img2, img3, img4, img5]
+  const [Choix, setChoix] = useState(0)
+  const HandClickCard = (index) => setChoix(index)
 
+  console.log(Choix)
   return (
-    <Fragment>
-
-      <nav>
-        <Link to='/Exercice01'>Exercice 01</Link>
-        <Link to='/Exercice02'>Exercice 02</Link>
-      </nav>
-
-      <Routes>
-        <Route exact path="/Exercice01" element={<Exercice01 />} />
-        <Route path="/Exercice02" element={<Exercice02/>} />
-        <Route path="*" element={<Exercice01 />} />
-      </Routes>
-
-    </Fragment>
-
+    <div className="main">
+      {
+        All_img.map((ele, index) =>
+          <div key={index}
+            className={Choix === index ? 'panel active' : 'panel'}
+            style={{
+              backgroundImage: `url(${ele})`
+            }}
+            onClick={() => HandClickCard(index)}>
+            <h3>Explore The World</h3>
+          </div>)
+      }
+    </div>
   );
 };
 
 export default App;
+
+
+
